49fd3122eab1ac0711bd94dc4b941fe925a07ba1
xixi <xixi_wen@foxmail.com>
2020-08-11 11:09:21 +0800

    Initial Commit

---
+++ XFSegment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XFSegment
+//
+//  Created by xixi_wen on 2020/8/11.
+//  Copyright © 2020 xixi_wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XFSegment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XFSegment
+//
+//  Created by xixi_wen on 2020/8/11.
+//  Copyright © 2020 xixi_wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
